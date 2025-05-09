44ebaa94d6e5f6a3d212b0780600f83c4ffc1f65
tuanemuy <22880537+tuanemuy@users.noreply.github.com>
2025-05-09 15:59:23 +0900

    fix: Fix exports

--- src/mod.ts
+++ src/mod.ts
@@ -2,7 +2,7 @@ import type { FileType } from "./core/types.ts";
 import { parse } from "./parser/index.ts";
 import { render } from "./renderer/index.ts";
 
-export * from "./core/types.ts";
+export type { CSVDoc, CSVRow, Tag, Attribute } from "./core/types.ts";
 export { parse } from "./parser/index.ts";
 export { render } from "./renderer/index.ts";
 
